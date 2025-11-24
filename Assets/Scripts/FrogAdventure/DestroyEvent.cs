189ce8e0919806b0aa32efb3c4f5f16f2bec3528
zhengpj <598359997@qq.com>
2025-11-24 15:50:57 +0800

    添加采集特效

---
+++ Assets/Scripts/FrogAdventure/DestroyEvent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEvent : MonoBehaviour
+{
+  // 销毁自身
+  public void DestroySelf()
+  {
+    Destroy(gameObject);
+  }
+}
--- Assets/Scripts/FrogAdventure/FruitCollect.cs
+++ Assets/Scripts/FrogAdventure/FruitCollect.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class FruitCollect : MonoBehaviour
 {
   public int scoreValue = 1;
+  public GameObject collectedPrefab;
 
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (other.CompareTag("Player"))
     {
       Destroy(gameObject);
+      AddCollectedAnimation(gameObject.transform);
       FruitCollectManager.Instance.ChangeScore(scoreValue);
       Debug.Log("Fruit collected: " + FruitCollectManager.Instance.Score);
     }
+  }
+
+  private void AddCollectedAnimation(Transform parent)
+  {
+    Instantiate(collectedPrefab, parent.position, Quaternion.identity);
   }
 }
