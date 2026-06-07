6549dd7845ad111013d3e3812bcfa9811f1dbd33
zhengpj <598359997@qq.com>
2026-06-07 16:25:36 +0800

    melee:添加掉落死亡销毁处理

--- Assets/Scripts/Melee/EnemyHealth.cs
+++ Assets/Scripts/Melee/EnemyHealth.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
   [SerializeField] private float maxHealth = 100f;
+  [SerializeField][Tooltip("Y 轴低于此值时判定为掉落死亡")]
+  private float fallDeathY = -10f;
 
   private EnemyChase enemyChase;
-
   private float currentHealth;
+  private bool hasFallenToDeath;
 
   void Start()
   {
     enemyChase = GetComponent<EnemyChase>();
     currentHealth = maxHealth;
+  }
+
+  void Update()
+  {
+    if (!hasFallenToDeath && transform.position.y <= fallDeathY)
+    {
+      hasFallenToDeath = true;
+      currentHealth = 0;
+      if (enemyChase != null)
+      {
+        enemyChase.ChangeState(EntityState.Death);
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
+    }
   }
 
   public void TakeDamage(float damage)
