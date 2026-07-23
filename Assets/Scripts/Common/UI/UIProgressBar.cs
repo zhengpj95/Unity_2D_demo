346523d322a58173b232265186db41b300acf4c1
zhengpj <598359997@qq.com>
2026-07-23 10:11:49 +0800

    refactor: 进度条UIProrgressBar优化

--- Assets/Scripts/Common/UI/UIProgressBar.cs
+++ Assets/Scripts/Common/UI/UIProgressBar.cs
@@ -1,7 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+
+public enum ProgressBarMode
+{
+  Fill,
+  Mask
+}
+public enum ProgressBarTextType
+{
+  Value,
+  Percent,
+  Custom
+}
 
 /**
  * 进度条UI组件，是底图+进度条图片+tmp文本
@@ -9,19 +22,25 @@ using TMPro;
 public class UIProgressBar : MonoBehaviour
 {
   [Header("References")]
-  [SerializeField] private RectTransform fill;    // imgProgress
-  [SerializeField] private TMP_Text label;        // tmpTxt
+  [SerializeField] private ProgressBarMode barMode = ProgressBarMode.Fill;
+  [SerializeField] private RectTransform img; // imgProgress
+  [SerializeField] private TMP_Text tmpTxt;   // tmpTxt
 
-  private float progress = 0f;                  // 进度(0~1)
-  private float _originalWidth;
+  private Image _fillImg;
+  private RectMask2D _mask2D;
+  private float _progress = 0f; // 进度(0~1)
 
   private void Start()
   {
-    if (fill != null)
+    if (barMode == ProgressBarMode.Fill)
     {
-      _originalWidth = fill.sizeDelta.x;
+      _fillImg = img.GetComponent<Image>();
     }
-    UpdateFill();
+    else if (barMode == ProgressBarMode.Mask)
+    {
+      _mask2D = img.GetComponent<RectMask2D>();
+    }
+    Reset();
   }
 
   /// <summary>
@@ -29,8 +48,8 @@ public class UIProgressBar : MonoBehaviour
   /// </summary>
   public void SetProgress(float value)
   {
-    progress = Mathf.Clamp01(value);
-    UpdateFill();
+    _progress = Mathf.Clamp01(value);
+    RefreshView();
   }
 
   /// <summary>
@@ -38,19 +57,34 @@ public class UIProgressBar : MonoBehaviour
   /// </summary>
   public void SetText(string text)
   {
-    if (label != null)
-      label.text = text;
+    if (tmpTxt != null)
+    {
+      tmpTxt.SetText(text);
+    }
   }
 
-  private void UpdateFill()
+  private void Reset()
   {
-    if (fill == null)
+    SetText("");
+    SetProgress(0);
+  }
+
+  private void RefreshView()
+  {
+    if (img == null)
     {
       return;
     }
 
-    Vector2 size = fill.sizeDelta;
-    size.x = _originalWidth * progress;
-    fill.sizeDelta = size;
+    if (barMode == ProgressBarMode.Fill)
+    {
+      _fillImg.fillAmount = _progress;
+    }
+    else if (barMode == ProgressBarMode.Mask)
+    {
+      Vector4 padding = _mask2D.padding;
+      padding.z = img.rect.width * (1 - _progress);
+      _mask2D.padding = padding;
+    }
   }
 }
