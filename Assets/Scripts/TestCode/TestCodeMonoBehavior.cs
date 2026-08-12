e3a954d236c396ddfe843121dc7f4180fd5f5634
zhengpj <598359997@qq.com>
2026-08-12 20:40:19 +0800

    test: protobuf测试内容

--- Assets/Scripts/TestCode/TestCodeMonoBehavior.cs
+++ Assets/Scripts/TestCode/TestCodeMonoBehavior.cs
@@ -4,28 +4,21 @@ using Google.Protobuf;
 
 public class TestCodeMonoBehavior : MonoBehaviour
 {
-  public void OnClickConfirm()
+  public async void OnClickConfirm()
   {
     // UIManager.Instance.OpenWindow<ConfirmPanel2Presenter>("Prefabs/ConfirmPanel2", UILayerIndex.Model, new { title = "提示", desc = "Are you sure? \nAre you close?" });
+    // UIManager.Instance.OpenWindow<AlertTipsPanelPresenter>("Prefabs/AlertTipsPanel", UILayerIndex.Model, new AlertTipsPanelArgs("警告标题", "警告信息！不允许随便修改！"));
 
-    UIManager.Instance.OpenWindow<AlertTipsPanelPresenter>("Prefabs/AlertTipsPanel", UILayerIndex.Model, new AlertTipsPanelArgs("警告标题", "警告信息！不允许随便修改！"));
+    c2s_user_login message = new c2s_user_login
+    {
+      AccountId = 1001,
+    };
+    await NetworkMgr.Instance.Send<c2s_user_login>(Cmd.C2S_USER_LOGIN, message);
   }
 
   // 测试 Protobuf 序列化和反序列化
-  public void OnTestProtobuf()
+  public async void OnTestProtobuf()
   {
-    // Example code to test Protobuf serialization and deserialization
-    c2s_user_register message = new c2s_user_register
-    {
-      UserName = "Test Message"
-    };
-
-    // Serialize the message to a byte array
-    byte[] serializedMessage = message.ToByteArray();
-
-    // Deserialize the byte array back to a message
-    c2s_user_register deserializedMessage = c2s_user_register.Parser.ParseFrom(serializedMessage);
-
-    Debug.Log($"Deserialized Message: UserName={deserializedMessage.UserName}");
+    await NetworkMgr.Instance.Connect("ws://localhost:3000");
   }
 }
