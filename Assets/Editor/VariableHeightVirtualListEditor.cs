6ce7158ec913a0d59a1441d379d081b51accfe02
zhengpj <598359997@qq.com>
2026-09-20 22:40:44 +0800

    feat(ui): add variable-height list inspector

---
+++ Assets/Editor/VariableHeightVirtualListEditor.cs
@@ -0,0 +1,45 @@
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace EditorTools
+{
+  [CustomEditor(typeof(VariableHeightVirtualList))]
+  public class VariableHeightVirtualListEditor : ScrollRectEditor
+  {
+    private SerializedProperty itemTemplate;
+    private SerializedProperty spacing;
+    private SerializedProperty estimatedItemHeight;
+    private SerializedProperty bufferHeight;
+    private SerializedProperty clickThreshold;
+    private SerializedProperty scrollSpeed;
+
+    protected override void OnEnable()
+    {
+      base.OnEnable();
+      itemTemplate = serializedObject.FindProperty("itemTemplate");
+      spacing = serializedObject.FindProperty("spacing");
+      estimatedItemHeight = serializedObject.FindProperty("estimatedItemHeight");
+      bufferHeight = serializedObject.FindProperty("bufferHeight");
+      clickThreshold = serializedObject.FindProperty("clickThreshold");
+      scrollSpeed = serializedObject.FindProperty("scrollSpeed");
+    }
+
+    public override void OnInspectorGUI()
+    {
+      serializedObject.Update();
+
+      EditorGUILayout.PropertyField(itemTemplate);
+      EditorGUILayout.Space();
+      EditorGUILayout.PropertyField(spacing);
+      EditorGUILayout.PropertyField(estimatedItemHeight);
+      EditorGUILayout.PropertyField(bufferHeight);
+      EditorGUILayout.Space();
+      EditorGUILayout.PropertyField(clickThreshold);
+      EditorGUILayout.PropertyField(scrollSpeed);
+
+      serializedObject.ApplyModifiedProperties();
+      EditorGUILayout.Space(10);
+      base.OnInspectorGUI();
+    }
+  }
+}
